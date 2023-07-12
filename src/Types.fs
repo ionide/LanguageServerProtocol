b4bf358f3c995aecc92116056a65874ed24ee079
Adam Tao <tcx4c70@gmail.com>
2023-07-12 23:39:45 +0800

    feat(3.15): Add DocumentLink#tooltip support
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Types.fs
+++ src/Types.fs
@@ -2444,7 +2444,18 @@ type DocumentLink =
     Range: Range
 
     /// The uri this link points to. If missing a resolve request is sent later.
-    Target: DocumentUri option }
+    Target: DocumentUri option
+
+    /// The tooltip text when you hover over this link.
+    /// If a tooltip is provided, is will be displayed in a string that includes
+    /// instructions on how to trigger the link, such as `{0} (ctrl + click)`.
+    /// The specific instructions vary depending on OS, user settings, and
+    /// localization.
+    Tooltip: string option
+
+    /// A data entry field that is preserved on a document link between a
+    /// DocumentLinkRequest and a DocumentLinkResolveRequest.
+    Data: JToken option }
 
 type DocumentColorParams =
   { /// The text document.
