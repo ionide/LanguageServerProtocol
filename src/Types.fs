a5cf482167e45c6dbbbf0ca2e52db8f568ddda21
Adam Tao <tcx4c70@gmail.com>
2023-07-19 20:30:17 +0800

    feat: Add Deprecated to DiagnosticTag
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Types.fs
+++ src/Types.fs
@@ -875,6 +875,10 @@ type DiagnosticTag =
   /// Clients are allowed to render diagnostics with this tag faded out instead of having
   /// an error squiggle.
   | Unnecessary = 1
+  /// Deprecated or obsolete code.
+  ///
+  /// Clients are allowed to rendered diagnostics with this tag strike through.
+  | Deprecated = 2
 
 type DiagnosticTagSupport =
   {
