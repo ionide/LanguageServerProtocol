6922d6667146646f865df7db8eeb414cb8b804d2
Chet Husk <chusk3@gmail.com>
2024-01-15 15:21:46 -0600

    Add in deprecations and some missing docs.

--- src/JsonRpc.fs
+++ src/JsonRpc.fs
@@ -43,14 +43,67 @@ type Notification =
     { Version = "2.0"; Method = method'; Params = rpcParams }
 
 module ErrorCodes =
+  open System
   let parseError = -32700
   let invalidRequest = -32600
   let methodNotFound = -32601
   let invalidParams = -32602
   let internalError = -32603
-  let serverErrorStart = -32000
-  let serverErrorEnd = -32099
+
+
+  ///<summary>This is the start range of JSON-RPC reserved error codes.
+  ///It doesn't denote a real error code. No LSP error codes should
+  ///be defined between the start and end range. For backwards
+  ///compatibility the <see cref="ServerNotInitialized"/> and the <see cref="UnknownErrorCode" />
+  ///are left in the range.</summary>
+  let jsonrpcReservedErrorRangeStart = -32099
+
+  [<Obsolete("Use jsonRpcReservedErrorRangeStart instead")>]
+  let serverErrorStart = jsonrpcReservedErrorRangeStart
+
+  /// Error code indicating that a server received a notification or
+  /// request before the server has received the `initialize` request.
+  let serverNotInitialized = -32002
+
+  ///This is the end range of JSON-RPC reserved error codes. It doesn't denote a real error code.
+  let jsonrpcReservedErrorRangeEnd = -32000
+
+  [<Obsolete("Use jsonRpcReservedErrorRangeEnd instead")>]
+  let serverErrorEnd = jsonrpcReservedErrorRangeEnd
+
+
+  /// This is the start range of LSP reserved error codes. It doesn't denote a real error code.
+  let lspReservedErrorRangeStart = -32899
+
+  /// A request failed but it was syntactically correct, e.g the
+  /// method name was known and the parameters were valid. The error
+  /// message should contain human readable information about why
+  /// the request failed.
+  /// @since 3.17.0
+  let RequestFailed = -32803
+
+
+  /// The server cancelled the request. This error code should
+  /// only be used for requests that explicitly support being
+  /// server cancellable.
+  ///
+  /// @since 3.17.0
+  let serverCancelled = -32802
+
+  /// The server detected that the content of a document got
+  /// modified outside normal conditions. A server should
+  /// NOT send this error code if it detects a content change
+  /// in it unprocessed messages. The result even computed
+  /// on an older state might still be useful for the client.
+  ///
+  /// If a client decides that a result is not of any use anymore
+  /// the client should cancel the request.
+  let contentModified = -32801
+
+  /// The client has canceled a request and a server has detected the cancel.
   let requestCancelled = -32800
+  /// This is the end range of LSP reserved error codes. It doesn't denote a real error code.
+  let lspReservedErrorRangeEnd = -32899
 
 type Error =
   { Code: int
