38beb5e65e3d23c3d4baea787d13c9929bfe236d
Jimmy Byrd <jimmy.byrd@motivity.net>
2024-09-13 08:02:29 -0400

    Update Fabulous Ast [1.0.0-pre16]

--- tools/MetaModelGenerator/GenerateTypes.fs
+++ tools/MetaModelGenerator/GenerateTypes.fs
@@ -28,21 +28,6 @@ module GenerateTypes =
     )
     |> String.concat ""
 
-  type ModuleOrNamespaceExtensions =
-    /// Allows Anonymous Module components to be yielded directly into a Module
-    /// Useful since there's no common holder of declarations or generic WidgetBuilder than can be used
-    /// when yielding different types of declarations
-    [<Extension>]
-    static member inline Yield(_: CollectionBuilder<'parent, ModuleDecl>, x: WidgetBuilder<AnonymousModuleNode>) =
-      let node = Gen.mkOak x
-
-      let ws =
-        node.Declarations
-        |> List.map (fun x -> Ast.EscapeHatch(x).Compile())
-        |> List.toArray
-        |> MutStackArray1.fromArray
-
-      { Widgets = ws }
 
   let JToken = LongIdent(nameof JToken)
 
@@ -440,7 +425,7 @@ module GenerateTypes =
     interfaceStructures
     |> Array.map (fun s ->
       let widget =
-        Interface($"I{s.Name}") {
+        TypeDefn($"I{s.Name}") {
           let properties = s.PropertiesSafe
 
           for p in properties do
@@ -453,7 +438,7 @@ module GenerateTypes =
                 p.Type
                 p
 
-            let ap = AbstractProperty(fi.Name, fi.TypeInfo)
+            let ap = AbstractSlot(fi.Name, fi.TypeInfo)
 
             yield
               fi.StructuredDocs
